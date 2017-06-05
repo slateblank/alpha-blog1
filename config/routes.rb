acaf37736830721f5d8cb31ed885e960f6509689
mattsmuh <matthew@slateblank.com>
2017-06-05 17:15:33 +0000

    Add user signup and edit and partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
     root 'pages#home'
     get 'about', to: 'pages#about'
-    
-  
-  
-  
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
+  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
